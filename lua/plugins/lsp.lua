ed28df7e882b667189756f563ec1b367a19b3cc6
Allan W. Nielsen <a@awn.dk>
2024-05-05 19:57:08 +0200

    adding cland extenions and disable lsp-formating

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -118,10 +118,66 @@ return {
                 on_attach = function(client, bufnr)
                     -- require("workspace-diagnostics").populate_workspace_diagnostics(vim.lsp.buf_get_clients()[1], 1)
                     -- require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
-                end
+                    client.server_capabilities.semanticTokensProvider = nil
+                end,
+
+                keys = {
+                    { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+                },
+                root_dir = function(fname)
+                    require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname)
+                end,
+                capabilities = {
+                    offsetEncoding = { "utf-16" },
+                },
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    "--completion-style=detailed",
+                    "--function-arg-placeholders",
+                    "--fallback-style=llvm",
+                },
+                init_options = {
+                    usePlaceholders = true,
+                    completeUnimported = true,
+                    clangdFileStatus = true,
+                },
+
             })
             -- lspconfig.vimls.setup({})
         end,
+    },
+
+    {
+        "p00f/clangd_extensions.nvim",
+        --lazy = true,
+        config = function() end,
+        opts = {
+            inlay_hints = {
+                inline = false,
+            },
+            ast = {
+                --These require codicons (https://github.com/microsoft/vscode-codicons)
+                role_icons = {
+                    type = "",
+                    declaration = "",
+                    expression = "",
+                    specifier = "",
+                    statement = "",
+                    ["template argument"] = "",
+                },
+                kind_icons = {
+                    Compound = "",
+                    Recovery = "",
+                    TranslationUnit = "",
+                    PackExpansion = "",
+                    TemplateTypeParm = "",
+                    TemplateTemplateParm = "",
+                    TemplateParamObject = "",
+                },
+            },
+        },
     },
 
     {
